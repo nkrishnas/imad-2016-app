d6280a90c948738e4ae39b5ee999d8f2ccb62ad5
nkrishnas <nkrishnas.2005@gmail.com>
2016-09-27 18:41:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -56,7 +56,13 @@ function createTemplate(data){
 
 }
 
-app.get('/', function (req, res) {
+var counter = 0;
+app.get('/', function (req,res){
+    counter = counter + 1;
+    res.send(counter.toString());
+});
+
+app.get('/counter', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
