14f4ddd657df521df2978f88a0ddb3b178d16617
nkrishnas <nkrishnas.2005@gmail.com>
2016-09-27 18:49:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -56,14 +56,16 @@ function createTemplate(data){
 
 }
 
-var counter = 0;
-app.get('/', function (req,res){
-    counter = counter + 1;
-    res.send(counter.toString());
+
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/counter', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+var counter = 0;
+app.get('/counter', function (req,res){
+    counter = counter + 1;
+    res.send(counter.toString());
 });
 
 app.get('/article-one', function (req, res) {
